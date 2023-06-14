40cf7fdfdc4573b67dd4aaf6237b007029db99f7
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-06-14 15:54:55 -0500

    Ejercicio 03 creado pero por confirmar
    Hay un pequeño error al momento de imprimir los valores.

---
+++ S01_Ejercicio03.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class S01_Ejercicio03 {
+    public static void main(String[] args) {
+        // ENTRADA
+        // Instanciar un objeto tipo Scanner
+        Scanner objTeclado = new Scanner(System.in);
+        // Leer el largo y el ancho desde el teclado
+        System.out.print("Largo: ");
+        int largo = objTeclado.nextInt();
+        System.out.print("Ancho: ");
+        int ancho = objTeclado.nextInt();
+        objTeclado.close();
+
+        // Instanciar mi clase Rectangulo
+        Rectangulo objRectangulo = new Rectangulo();
+
+        //Asignar valores a las propiedades
+        objRectangulo.largo = largo;
+        objRectangulo.largo = ancho;
+
+        // Ejecutar el método
+        objRectangulo.imprimirResultados();
+    }
+}

bb42da91fa144107ece56c581d640d1d47e4ae21
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-06-14 16:10:58 -0500

    Update S01_Ejercicio04.java
    Completo y funcional.

--- S01_Ejercicio04.java
+++ S01_Ejercicio04.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class S01_Ejercicio04 {
     public static void main(String[] args) {
+        Scanner objTeclado = new Scanner(System.in);
+        Rectangulo objRectangulo = new Rectangulo();
         
+        System.out.print("Largo: ");
+        objRectangulo.largo = objTeclado.nextInt();
+        System.out.print("Ancho: ");
+        objRectangulo.ancho = objTeclado.nextInt();
+        objTeclado.close();
+
+        System.out.println("Perimetro:\t" + objRectangulo.getPerimetro());
+        System.out.println("Area:\t" + objRectangulo.getArea());
+        System.out.println("Diagonal:\t" + objRectangulo.getDiagonal());
     }
 }

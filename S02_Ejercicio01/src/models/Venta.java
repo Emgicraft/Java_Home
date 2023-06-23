73d6172444e95510d282f50b643610014398db0b
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-06-22 19:46:08 -0500

    Clase Venta completado.

--- S02_Ejercicio01/src/models/Venta.java
+++ S02_Ejercicio01/src/models/Venta.java
@@ -8,4 +8,47 @@ public class Venta {
     // Propiedades
     Producto producto;
     int cantidad;
+    
+    // Constructores
+    public Venta() {
+    }
+    
+    public Venta(Producto prod, int cant) {
+        this.producto = prod;
+        this.cantidad = cant;
+    }
+    
+    // Métodos
+    public double total() {
+        return this.producto.getPrecio() * this.cantidad;
+    }
+    
+    // Getters y Setters
+    /**
+     * @return El producto
+     */
+    public Producto getProducto() {
+        return this.producto;
+    }
+
+    /**
+     * @param producto El producto a fijar
+     */
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    /**
+     * @return La cantidad
+     */
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    /**
+     * @param cantidad La cantidad a fijar
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
